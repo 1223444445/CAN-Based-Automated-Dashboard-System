f38c0a7f86209e5aec7a7f867c6fc084763d80f9
1223444445 <166110626+1223444445@users.noreply.github.com>
2026-06-13 00:44:44 +0530

    model_2

--- can.c
+++ can.c
@@ -1,11 +1,11 @@
 /*File for CAN configuration , setting message id's, Data Transmission, */
 #include <xc.h>
 #include "can.h"
-#include "clcd.h"
+//#include "clcd.h"
 
 /* CAN operation mode values*/
 typedef enum _CanOpMode {
-    /* Use this to access opcode bits */
+    /* Use this to access opmode bits */
     e_can_op_mode_normal = 0x00,
     e_can_op_mode_loop = 0x40,
     e_can_op_mode_config = 0x80
@@ -17,11 +17,11 @@ void init_can(void) {
     TRISB2 = 0; /* CAN TX */
     TRISB3 = 1; /* CAN RX */
 
-    /* Enter CAN module into configuration mode */
+    /* Enter CAN module into config mode */
     /* clear previous mode */
     CAN_SET_OPERATION_MODE_NO_WAIT(e_can_op_mode_config); /* set new mode */
 
-    /* Wait until desired mode is set */
+    /* Wait untill desired mode is set */
     while (CANSTAT != 0x80);
 
     /* Enter CAN module into Mode 0 */
@@ -53,32 +53,30 @@ static uint16_t get_msg_id_std(void) {
 }
 
 /*function for the setting the message id's*/
-static void set_msg_id_std(unsigned int id) {// set 8bit to 11bits
+static void set_msg_id_std(unsigned int id) {
     TXB0SIDL = (id & 0x7) << 5;
     TXB0SIDH = (id >> 3);
 }
 
 /*DATA transmission function for Speed and Gear change*/
 
-void can_transmit(uint16_t msg_id, const uint8_t *data, uint8_t len) 
-{
+void can_transmit(uint16_t msg_id, const uint8_t *data, uint8_t len) {
     uint8_t *ptr;
     TXB0EIDH = 0x00; /* Extended Identifier */
     TXB0EIDL = 0x00; /* Extended Identifier */
 
     // Set MSG ID
-    set_msg_id_std(msg_id); // set 8bit to 11bits
+    set_msg_id_std(msg_id);
 
     // Set data length
-    TXB0DLC = len;//if we send only 2bytes so send the size as 2bytes
+    TXB0DLC = len;
     /* Send the data by writing the bytes to individual 
      *  TXB0D0, TXB0D1 .... TXB0D7
-     * Max length should be 8 (No error handing here :(
+     * Max len should be 8 (No error handing here :(
      * */
     ptr = (uint8_t *) & TXB0D0;
-    for (int i = 0; i < len; i++) {//transmitting the data if we send 2bytes until length time
-        ptr[i] = data[i];       //eg; if 2bytes ptr[0] = data[0] , ptr[1] = data[1];
-        
+    for (int i = 0; i < len; i++) {
+        ptr[i] = data[i];
     }
     TXB0REQ = 1; /* Set the buffer to transmit */
 }
@@ -87,7 +85,7 @@ void can_transmit(uint16_t msg_id, const uint8_t *data, uint8_t len)
  * len can be zero if no data is available
  * Mandatory for caller to check len before processing
  * */
-void can_receive(uint16_t *msg_id, uint8_t *data, uint8_t *len) {//all the parameters are empty because we will receive the data from tx
+void can_receive(uint16_t *msg_id, uint8_t *data, uint8_t *len) {
     uint8_t *ptr;
 
     if (RXB0FUL) /* CheckRXB0 */ {
@@ -107,5 +105,5 @@ void can_receive(uint16_t *msg_id, uint8_t *data, uint8_t *len) {//all the param
     }
 
     // No data available.
-    //*len = 0;
+   // *len = 0;
 }
--- digital_keypad.h
+++ digital_keypad.h
@@ -1,43 +1,20 @@
-#ifndef MATRIX_KEYPAD_H
-#define MATRIX_KEYPAD_H
+#ifndef DIGITAL_KEYPAD_H
+#define DIGITAL_KEYPAD_H
 
+#define LEVEL					     	0
+#define STATE_CHANGE			       	1
 
-	
-#define MAX_ROW				4
-#define MAX_COL				3
+#define KEY_PORT					PORTC
 
-#define STATE_CHANGE				1
-#define LEVEL_CHANGE				0
-#define MATRIX_KEYPAD_PORT			PORTB
-#define ROW3					PORTBbits.RB7
-#define ROW2					PORTBbits.RB6
-#define ROW1					PORTBbits.RB5
-#define COL4					PORTBbits.RB4
-#define COL3					PORTBbits.RB3
-#define COL2					PORTBbits.RB2
-#define COL1					PORTBbits.RB1
+#define SWITCH1					0x0E
+#define SWITCH2					0x0D
+#define SWITCH3					0x0B
+#define SWITCH4					0x07
+#define ALL_RELEASED					0x0F
 
+#define INPUT_PINS					0x0F
 
-#define MK_SW1					1
-#define MK_SW2					2
-#define MK_SW3					3
-#define MK_SW4					4
-#define MK_SW5					5
-#define MK_SW6					6
-#define MK_SW7					7
-#define MK_SW8					8
-#define MK_SW9					9
-#define MK_SW10				10
-#define MK_SW11				11
-#define MK_SW12				12
-
-#define ALL_RELEASED	0xFF
-
-#define HI				1
-#define LO				0
-
-void init_matrix_keypad(void);
-unsigned char scan_key(void);
-unsigned char read_switches(unsigned char detection_type);
+void init_digital_keypad(void);
+unsigned char read_digital_keypad(unsigned char detection_type);
 
 #endif
